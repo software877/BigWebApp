220d2f72baaabd19bc2d5e0b2b9004a76106a76e
test <testset@fdsfds.com>
2023-09-23 23:12:00 +0300

    the test view was added

--- big_web_app/polls/urls.py
+++ big_web_app/polls/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:user_id>/results/", views.results, name="results"),
 ]
--- big_web_app/polls/views.py
+++ big_web_app/polls/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import BetaUser
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = BetaUser.objects.order_by("-reg_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def results(request, user_id):
+    response = "You're looking at the results of user %s."
+    return HttpResponse(response % user_id)
